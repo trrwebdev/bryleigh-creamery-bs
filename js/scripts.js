6bdf36367c23df1314f8273926b73f9f5a5b897f
Terri Read <trrwebdev@gmail.com>
2022-12-10 22:53:32 -0600

    catering page

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(function() {
+    
+    $('#couponButton').click(function(){
+        $('#couponModal').modal('show');
+        } );
+    $('#loginButton').click(function(){
+        $('#loginModal').modal('show');
+        } );
+});
